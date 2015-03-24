62d49044cbc45a21f28e93be65dabc5e1b1fa416
CriseX <markuwil@users.sourceforge.net>
2015-03-24 03:30:25 +0200

    remaining DB table defs

--- applications/movies/models/movie.php
+++ applications/movies/models/movie.php
@@ -3,7 +3,7 @@
 /**
 *
 * @package svntools
-* @version $Id: movie.php 1199 2015-03-23 21:41:57Z crise $
+* @version $Id: movie.php 1201 2015-03-24 01:55:10Z crise $
 * @copyright (c) 2014 Markus Willman, markuwil <at> gmail <dot> com / www.apexdc.net
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
@@ -33,5 +33,34 @@ class movies_movie_model extends web_model
 
 			PRIMARY KEY (movie_id)
 		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin");
+
+		$db->update("
+		CREATE TABLE IF NOT EXISTS movies_screenings (
+			screening_id mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+			screening_start int(11) unsigned NOT NULL DEFAULT 0,
+			screening_end int(11) unsigned NOT NULL DEFAULT 0,
+
+			movie_id mediumint(8) unsigned NOT NULL,
+			room_id mediumint(8) unsigned NOT NULL,
+
+			PRIMARY KEY (screening_id),
+			KEY (movie_id),
+			KEY (room_id)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin");
+
+		$db->update("
+		CREATE TABLE IF NOT EXISTS movies_reservations (
+			cords_seat tinyint(3) unsigned NOT NULL,
+			cords_row tinyint(3) unsigned NOT NULL,
+			reservation_state tinyint(1) unsigned NOT NULL DEFAULT 0,
+
+			screening_id mediumint(8) unsigned NOT NULL,
+			user_id mediumint(8) unsigned NOT NULL,
+
+			PRIMARY KEY reservation_key (screening_id, cords_seat, cords_row),
+			KEY transaction_key (screening_id, user_id)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin");
 	}
+
+
 }
--- applications/movies/models/theater.php
+++ applications/movies/models/theater.php
@@ -3,7 +3,7 @@
 /**
 *
 * @package svntools
-* @version $Id: theater.php 1199 2015-03-23 21:41:57Z crise $
+* @version $Id: theater.php 1200 2015-03-24 01:51:24Z crise $
 * @copyright (c) 2014 Markus Willman, markuwil <at> gmail <dot> com / www.apexdc.net
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
@@ -31,5 +31,20 @@ class movies_theater_model extends web_model
 
 			PRIMARY KEY (theater_id)
 		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin");
+
+		$db->update("
+		CREATE TABLE IF NOT EXISTS movies_rooms (
+			room_id mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+			room_name varchar(255) NOT NULL,
+			room_seats tinyint(3) unsigned NOT NULL,
+			room_rows tinyint(3) unsigned NOT NULL,
+
+			theater_id mediumint(8) unsigned NOT NULL,
+
+			PRIMARY KEY (room_id),
+			KEY (theater_id)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin");
 	}
+
+
 }
