425b98c97711a19fbb83b768f95343383db28836
Matti Iso-Järvenpää <mjisja@utu.fi>
2015-03-27 19:57:08 +0200

    Screenings methods

--- applications/movies/models/screening.php
+++ applications/movies/models/screening.php
@@ -37,6 +37,111 @@ class movies_screening_model extends web_model
 			KEY (room_id)
 		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin");
 	}
+	
+	public function add_screening(array $meta_data)
+	{
+		return ($this->database->update($this->database->build_insert('movie_screenings', array(
+			'screening_id'			=> (int) $meta_data['screening_id'],
+			'screening_start'		=> (int) $meta_data['start'],
+			'screening_end'			=> (int) $meta_data['end'],
+			'movie_id'				=> (int) $meta_data['movie_id'],
+			'room_id'				=> (int) $meta_data['room_id'],
+		))) == 1);
+	}
+
+	public function update_screening($screening_id, array $meta_data)
+	{
+		$update_fields = array();
+
+		if (isset($meta_data['start']))
+			$update_fields['screening_start'] = (int) $meta_data['start'];
+		if (isset($meta_data['end']))
+			$update_fields['screening_end'] = (int) $meta_data['end'];
+		if (isset($meta_data['movie_id']))
+			$update_fields['movie_id'] = (int) $meta_data['movie_id'];
+		if (isset($meta_data['room_id']))
+			$update_fields['room_id'] = (int) $meta_data['room_id'];
+
+		return ($this->database->update($this->database->build_update('movie_screenings', $update_fields, 'screening_id = '. (int) $screening_id)) == 1);
+	}
+
+	public function remove_screening($screening_id)
+	{
+		$conds = array();
+		$conds[] = 'screening_id = '. (int) $screening_id;
+		$conds[] = 'NOT EXISTS (SELECT * FROM movie_screenings WHERE screening_id = ' . (int) $screening_id . ')';
+
+		return ($this->database->update($this->database->build_delete('movie_screenings', $conds)) == 1);
+	}
+
+	public function count_screenings($only_upcoming = true)
+	{
+		$conds = array();
+		$conds[] = ($only_upcoming ? 'NOT EXISTS (SELECT * FROM movie_screenings WHERE screening_start < ' . time() . ')' : false);
+
+		$this->database->query('SELECT COUNT(ms.screening_id) AS screenings FROM movie_screenings AS ms ' . $this->database->build_where($conds));
+
+		$row = $this->database->fetchRow();
+		$this->database->freeResult();
+		return $row['screenings'];
+	}
+
+	public function get_screenings($only_upcoming = true, $limit = 15, $offset = 0)
+	{
+		$this->database->limitQuery("
+			SELECT		ms.screening_id, ms.screening_start AS start, ms.screening_end AS end, ms.movie_id AS movie_id, ms.room_id AS room_id
+			FROM		movie_screenings AS ms 
+			WHERE		" .(only_upcoming ? ' HAVING screening_start > ' . time() : '') . "
+			ORDER BY	ms.screening_start ASC", $limit, $offset);
+
+		$screenings = array();
+		while (($row = $this->database->fetchRow()) !== false)
+			$screenings[] = $row;
+
+		$this->database->freeResult();
+		return $screenigs;
+	}
+	
+	public function get_screenings_user($theater_id, $movie_id = null ,$only_upcoming = true, $limit = 15, $offset = 0)
+	{
+		$this->database->limitQuery("
+			SELECT		ms.screening_id, ms.screening_start AS start, ms.movie_name AS movie_name, mr.room_name AS room_name
+			FROM		movie_screenings AS ms LEFT JOIN movie_rooms AS mr ON ms.room_id = mr.room_id
+			LEFT JOIN 	movie_info AS mi ON mi.movie_id = ms.movie_id
+			WHERE		mr.theater_id = " . $theater_id . (only_upcoming ? 'AND HAVING screening_start > ' . time() : '') . ((movie_id != null) ? 'AND ms.movie_id = ' . $movie_id : ''). "
+			ORDER BY	ms.screening_start ASC", $limit, $offset);
+
+		$screenings = array();
+		while (($row = $this->database->fetchRow()) !== false)
+			$screenings[] = $row;
+
+		$this->database->freeResult();
+		return $screenigs;
+	}
+	
+	public function count_seats($screening_id)
+	{
+		$this->database->Query("
+			SELECT		mr.room_seats AS seats, mr.room_rows AS rows
+			FROM		movie_rooms AS mr LEFT JOIN movie_screenings AS ms ON mr.room_id = ms.room_id
+			WHERE		ms.screening_id = " .($screening_id));
+			
+		$row = $this->database->fetchRow();
+		$this->database->freeResult();
+		return ($row['seats']*$row['rows']);
+	}
+	
+	public function count_free_seats($screening_id)
+	{
+		$this->database->Query("
+			SELECT		COUNT(mr.reservation_id) AS freeseats
+			FROM		movie_reservations AS mr 
+			WHERE		mr.screening_id = " .($screening_id));
+			
+		$row = $this->database->fetchRow();
+		$this->database->freeResult();
+		return ($this->count_seats($screening_id)-$row['freeseats']);
+	}
 
 
 }
