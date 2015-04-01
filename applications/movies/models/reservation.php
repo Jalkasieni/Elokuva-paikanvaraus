12251a7a61ebcecb9d3b6066a3cec11b56e855eb
CriseX <markuwil@users.sourceforge.net>
2015-04-01 16:42:51 +0300

    fix... once more

--- applications/movies/models/reservation.php
+++ applications/movies/models/reservation.php
@@ -3,7 +3,7 @@
 /**
 *
 * @package svntools
-* @version $Id: reservation.php 1295 2015-04-01 13:36:43Z crise $
+* @version $Id: reservation.php 1296 2015-04-01 13:42:02Z crise $
 * @copyright (c) 2014 Markus Willman, markuwil <at> gmail <dot> com / www.apexdc.net
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
@@ -111,7 +111,7 @@ class movies_reservation_model extends web_model
 		for ($i = 1; $i <= $size['rows']; ++$i)
 		{
 			for ($j = 1; $j <= $size['seats']; ++$j)
-				$table[$i] = array('state' => self::STATE_FREE, 'user_id' => 0);
+				$table[$i][$j] = array('state' => self::STATE_FREE, 'user_id' => 0);
 		}
 
 		$this->database->query("
